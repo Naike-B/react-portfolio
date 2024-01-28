d68d8b2ae924ac1f1bbc7d39496c496801f86246
Kiki <146773290+Naike-B@users.noreply.github.com>
2024-01-28 16:13:36 +0000

    "Initialise project"

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return <h1> My portfolio </h1>;
+}
+
+export default App;
